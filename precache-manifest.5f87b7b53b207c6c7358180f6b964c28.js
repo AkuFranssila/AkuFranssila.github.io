9b2764fec695e443fcdd89a6c5b845da4988e3f7
AkuFranssila <akufra@gmail.com>
2020-08-09 15:11:53 +0300

    Updates

---
+++ precache-manifest.5f87b7b53b207c6c7358180f6b964c28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eceacc5ba0571431940f8b40ec09c0db",
+    "url": "/AkuFranssila.github.io/index.html"
+  },
+  {
+    "revision": "23621e1264a0055ac0b6",
+    "url": "/AkuFranssila.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "78128a40c511654b994b",
+    "url": "/AkuFranssila.github.io/static/js/2.530322f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AkuFranssila.github.io/static/js/2.530322f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23621e1264a0055ac0b6",
+    "url": "/AkuFranssila.github.io/static/js/main.25eb94e7.chunk.js"
+  },
+  {
+    "revision": "3e2ffb326e036fe95c25",
+    "url": "/AkuFranssila.github.io/static/js/runtime-main.da2027d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AkuFranssila.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
